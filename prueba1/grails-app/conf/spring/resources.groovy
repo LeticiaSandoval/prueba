f7733fb063549108c2cd08a0ae4fe7ab9dee9127
lulaluna <letysan2932@gmail.com>
2018-11-14 13:08:22 -0600

    se borro archivos

--- prueba1/grails-app/conf/spring/resources.groovy
+++
@@ -1,7 +0,0 @@
-import prueba1.security.UserPasswordEncoderListener
-import prueba1.security.UserPasswordEncoderListener
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-}
